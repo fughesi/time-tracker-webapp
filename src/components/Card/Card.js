edb5fbe08e751b9d0f8ae6dfa82547e5bafed333
Steven <steven.camillieri@protonmail.com>
2022-01-27 10:08:31 -0500

    media queries

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Ellipsis from "../../images/oldIcons/icon-ellipsis.svg";
-import workIcon from "../../images/oldIcons/icon-work.svg";
+// import Ellipsis from "../../images/oldIcons/icon-ellipsis.svg";
+// import workIcon from "../../images/oldIcons/icon-work.svg";
 import "./Card.css";
 
 export default function Card(props) {
@@ -11,7 +11,14 @@ export default function Card(props) {
       <div id="cardBottom">
         <section id="cardContainer">
           <h3>{props.activity}</h3>
-          <img src={Ellipsis} alt="Ellipsis" id="Ellipsis" />
+          <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+              fill="#BBC0FF"
+              fillRule="evenodd"
+            />
+          </svg>
+          {/* <img src={Ellipsis} alt="Ellipsis" id="Ellipsis" /> */}
         </section>
 
         <section id="valueContainer">
